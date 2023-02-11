a46eae7ffed5cfa8d050d1cecef949ef4273d16c
Christopher Chang <chiggiechang@gmail.com>
2023-02-11 12:01:10 -0800

    Improve styling of error display

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="typescript">
-  import { lastError } from "$lib/stores";
+  import { lastError } from '$lib/stores';
   import JSONTree from 'svelte-json-tree';
 </script>
 
 {#if $lastError}
-  <JSONTree value={$lastError} />
+  <div style="--json-tree-font-family: monospace;">
+    <JSONTree value={$lastError} />
+  </div>
 {/if}
 
 <slot />
