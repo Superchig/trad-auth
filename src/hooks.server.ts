35ebf2ddefe7b8d86de237d94e36a06a0581a6ec
Christopher Chang <chiggiechang@gmail.com>
2023-02-12 17:15:33 -0800

    Add HTTP request logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,23 @@
 import { createContext } from '$lib/trpc/context';
 import { router } from '$lib/trpc/router';
 import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 import { createTRPCHandle } from 'trpc-sveltekit';
 
-export const handle: Handle = createTRPCHandle({ router, createContext });
+const tRPCHandle = createTRPCHandle({ router, createContext });
+
+const httpLoggerHandle: Handle = async ({ event, resolve }) => {
+  const requestStartTime = Date.now();
+  const response = await resolve(event);
+
+  console.log(
+    new Date(requestStartTime).toISOString(),
+    event.request.method,
+    event.url.pathname,
+    `(${Date.now() - requestStartTime}ms)`,
+    response.status
+  )
+  return response;
+}
+
+export const handle: Handle = sequence(tRPCHandle, httpLoggerHandle);
