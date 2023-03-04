3b0ae89fd06a5a18b2101d52d14db563d7799c41
Christopher Chang <chiggiechang@gmail.com>
2023-03-04 14:35:58 -0800

    Use Atkinson Hyperlegible as the font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Atkinson Hyperlegible',
+          'ui-sans-serif',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji'
+        ]
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
